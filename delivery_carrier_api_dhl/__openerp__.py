c74cdc6b9817bba48b739678de60abf9ea57d774
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-10-01 17:28:55 +0200

    + add very slopy dhl api support

---
+++ delivery_carrier_api_dhl/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Delivery Carrier API: DHL',
+    'version': '1.0',
+    'category': 'Generic Modules/Warehouse',
+    'description': """
+Sub-module for Base Delivery Carrier Files.
+
+Definition of the delivery carrier api for "DHL".
+
+    """,
+    'author': 'Impulzia S.L',
+    'license': 'AGPL-3',
+    'website': 'http://www.impulzia.com',
+    'depends': ['base_delivery_carrier_files', 'stock'],
+    'init_xml': [],
+    'data': ['views/carrier_file_view.xml'],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+}
