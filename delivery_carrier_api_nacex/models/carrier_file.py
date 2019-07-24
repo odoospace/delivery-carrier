253ee4fbabb7a7d9a5282fd7db1d09a7d570c2b3
Javier Colmeiro <javier.colmeiro@impulzia.com>
2019-07-24 18:34:39 +0200

    + add new module as POC for nacex api integration

---
+++ delivery_carrier_api_nacex/models/carrier_file.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, exceptions
+import nacex
+
+
+class CarrierFile(models.Model):
+    _inherit = 'delivery.carrier.file'
+
+    @api.model
+    def get_type_selection(self):
+        result = super(CarrierFile, self).get_type_selection()
+        if 'NACEX' not in result:
+            result.append(('NACEX', 'Envíos NACEX-API'))
+        return result
+
+
+    type = fields.Selection(get_type_selection, 'Type', required=True)
+    nacex_api_debug = fields.Boolean('Nacex api Debug flag', default=False)
+    nacex_api_pwd = fields.Char('Nacex api pass')
+    nacex_api_user = fields.Char('Nacex api User')
+    nacex_api_url = fields.Char('Nacex api URL')
+    nacex_api_del_cli = fields.Char('Nacex api del_cli')
+    nacex_api_num_cli = fields.Char('Nacex api num_cli')
+    nacex_api_tip_ser = fields.Char('Nacex api tip_ser', default='27')
+    nacex_api_tip_cob = fields.Char('Nacex api tip_cob', default='O')
+
+
+CarrierFile()
+
+
+class stock_picking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def action_done(self):
+        if self.carrier_id:
+            if self.carrier_id.name == 'NACEX API':
+                d = nacex.API()
+                if self.number_of_packages == 1:
+                    data = {
+                        'del_cli': self.carrier_id.carrier_file_id.nacex_api_del_cli,
+                        'num_cli': self.carrier_id.carrier_file_id.nacex_api_num_cli,
+                        'fec': fields.Datetime.now().strftime('%d/%m/%Y'),
+                        'tip_ser': self.carrier_id.carrier_file_id.nacex_api_tip_ser,
+                        'tip_cob': self.carrier_id.carrier_file_id.nacex_api_tip_cob,
+                        'exc': '0',
+                        'ref_cli': 'INFORMATICA',
+                        'tip_env': '2',
+                        'bul': '1',
+                        'kil': '2',
+                        'nom_ent': self.partner_id.name,
+                        'per_ent': self.partner_id.name,
+                        'dir_ent': self.partner_id.street,
+                        'pais_ent': self.partner_id.country_id.code,
+                        'cp_ent': self.partner_id.zip,
+                        'pob_ent': self.partner_id.city,
+                        'tel_ent': self.partner_id.phone or self.partner_id.mobile,
+                    }
+
+                    if self.sale_id:  
+                        if self.sale_id.payment_mode_id.id == 4 :
+                            data["tip_cob"] = "D" 
+                            data['ree'] = self.sale_id.amount_total
+                    ok_generated = False
+                    res = n.putExpedicion(data=data)
+                    if 'data' in res:
+                        if 'exp_code' in res['data']:
+                            exp_code = res['data']['exp_cod']
+                            ok_generated = True
+                    if not ok_generated:
+                        raise exceptions.Warning(("NACEX API ERROR: Creating shipment... %s" % (res)))
+                        return
+                    
+                    data = {
+                        'codExp': exp_code,
+                        'modelo': 'IMAGEN'
+                    }
+                    ok_label = False
+                    tracking = api.getEtiqueta(data=data)
+                    if 'data' in tracking:
+                        if 'etiqueta' in tracking['data']:
+                            ok_label = True
+                    if not ok_label:
+                        raise exceptions.Warning(("NACEX API ERROR: Generating label... %s" % (res)))
+                        return
+
+                    #save attachment
+                    attachment = {}
+                    attachment['name'] = exp_code
+                    attachment['datas_fname'] = attachment['name'] + '.pdf'
+                    attachment['res_model'] = 'stock.picking'
+                    attachment['res_id'] = self.id
+                    attachment['datas'] = tracking['data']['etiqueta']
+                    attachment['file_type'] ='application/pdf'
+                    att = self.env['ir.attachment'].create(attachment)
+
+                    self.carrier_file_generated = True
+                    self.carrier_tracking_ref = exp_code
+                else:
+                    raise exceptions.Warning(("NACEX API ERROR: You must set a number of packages = 1"))
+        result = super(stock_picking, self).action_done()
+        return result
+
+    @api.multi
+    def do_transfer(self):
+        if self.carrier_id:
+            if self.carrier_id.name == 'NACEX API':
+                d = nacex.API()
+                if self.number_of_packages == 1:
+                    data = {
+                        'del_cli': self.carrier_id.carrier_file_id.nacex_api_del_cli,
+                        'num_cli': self.carrier_id.carrier_file_id.nacex_api_num_cli,
+                        'fec': fields.Datetime.now().strftime('%d/%m/%Y'),
+                        'tip_ser': self.carrier_id.carrier_file_id.nacex_api_tip_ser,
+                        'tip_cob': self.carrier_id.carrier_file_id.nacex_api_tip_cob,
+                        'exc': '0',
+                        'ref_cli': 'INFORMATICA',
+                        'tip_env': '2',
+                        'bul': '1',
+                        'kil': '2',
+                        'nom_ent': self.partner_id.name,
+                        'per_ent': self.partner_id.name,
+                        'dir_ent': self.partner_id.street,
+                        'pais_ent': self.partner_id.country_id.code,
+                        'cp_ent': self.partner_id.zip,
+                        'pob_ent': self.partner_id.city,
+                        'tel_ent': self.partner_id.phone or self.partner_id.mobile,
+                    }
+
+                    if self.sale_id:  
+                        if self.sale_id.payment_mode_id.id == 4 :
+                            data["tip_cob"] = "D" 
+                            data['ree'] = self.sale_id.amount_total
+                    ok_generated = False
+                    res = n.putExpedicion(data=data)
+                    if 'data' in res:
+                        if 'exp_code' in res['data']:
+                            exp_code = res['data']['exp_cod']
+                            ok_generated = True
+                    if not ok_generated:
+                        raise exceptions.Warning(("NACEX API ERROR: Creating shipment... %s" % (res)))
+                        return
+                    
+                    data = {
+                        'codExp': exp_code,
+                        'modelo': 'IMAGEN'
+                    }
+                    ok_label = False
+                    tracking = api.getEtiqueta(data=data)
+                    if 'data' in tracking:
+                        if 'etiqueta' in tracking['data']:
+                            ok_label = True
+                    if not ok_label:
+                        raise exceptions.Warning(("NACEX API ERROR: Generating label... %s" % (res)))
+                        return
+
+                    #save attachment
+                    attachment = {}
+                    attachment['name'] = exp_code
+                    attachment['datas_fname'] = attachment['name'] + '.pdf'
+                    attachment['res_model'] = 'stock.picking'
+                    attachment['res_id'] = self.id
+                    attachment['datas'] = tracking['data']['etiqueta']
+                    attachment['file_type'] ='application/pdf'
+                    att = self.env['ir.attachment'].create(attachment)
+
+                    self.carrier_file_generated = True
+                    self.carrier_tracking_ref = exp_code
+                else:
+                    raise exceptions.Warning(("NACEX API ERROR: You must set a number of packages = 1"))
+        result = super(stock_picking, self).do_transfer()
+        return result
