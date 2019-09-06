30af3e3727253b39d587c848798989b72b92590d
Javier Colmeiro <javier.colmeiro@impulzia.com>
2019-09-06 13:29:50 +0200

    + fix imports

--- delivery_carrier_printer/models/carrier_file.py
+++ delivery_carrier_printer/models/carrier_file.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api, exceptions
-import nacex
-from datetime import datetime
-import base64
-import io
-from PIL import Image
-import subprocess
-import tempfile
-import pdfkit
-
 import logging
 
 _logger = logging.getLogger(__name__)
